e4495642d0fd6e29fc84d0121e32f1a96956192b
Shradha Pulla & Wolfgang Warneke <pullashradha@gmail.com>
2016-07-19 09:30:48 -0700

    Create database and tables, add scripts file to project folder

--- Tests/StudentTest.cs
+++ Tests/StudentTest.cs
@@ -1,20 +1,20 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Data;
-// using System.Data.SqlClient;
-// using Xunit;
-//
-// namespace UniversityRegistrar
-// {
-//   public class StudentTest : IDisposable
-//   {
-//     public StudentTest()
-//     {
-//       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=university_registrar__test;Integrated Security=SSPI;";
-//     }
-//     public void Dispose()
-//     {
-//       Student.DeleteAll();
-//     }
-//   }
-// }
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace UniversityRegistrar
+{
+  public class StudentTest : IDisposable
+  {
+    public StudentTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=university_registrar__test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      Student.DeleteAll();
+    }
+  }
+}
